0f03b08d4ce4cd9385e6275608cb09eac1b34ecf
jek55rus jek55rus <markin95evgeny@gmail.com>
2020-01-14 02:46:10 +0300

    fixed pawn manipulation

--- Pawn.py
+++ Pawn.py
@@ -1,28 +1,41 @@
 class Pawn:
-	'''
-	just a notation clarification: pawn = chip
-	The Pawn class handles any manipulation on the pawns produced by a player
-	'''
-    def __init__(self, char, path, board):
+    '''
+    just a notation clarification: pawn = chip
+    The Pawn class handles any manipulation on the pawns produced by a player
+    '''
+    def __init__(self, char, path):
         self.char = char # a character the pawn is represented by 
-        self.path = path(board, char) # Path class
+        self.path = path # Path class
         self.current_position_idx = 0 # None by default
+        self.Ds = self.path.p
+        self.is_in_D = False
     
     def show_pawns_path(self):
         return self.path.get_path() # display the whole path for the pawn
     
     def move(self, step):
-        self.current_position_idx += step 
+        self.check_in_D()
+        if self.is_in_D:
+            self.current_position_idx += 0
+            print('this pawn has already reached the D-zone')
+        elif self.current_position_idx + step >= len(self.path.get_path()):
+            self.current_position_idx += 0
+            print('this pawn cannot be moved since it will go outside in this case')
+        else:
+            self.current_position_idx += step 
         
     def get_current_position(self):
         return self.path.get_path()[self.current_position_idx]
         
-    def is_in_D(self):
-		# check if the pawn is in D-field already
-        return False if self.current_position_idx < len(self.path.get_path()) - self.Ds - 1 else True
+    def check_in_D(self):
+        # check if the pawn is in D-field already
+        # print('entered check_in_D')
+        # print('self.current_position_idx', self.current_position_idx, 'len(self.path.get_path())', len(self.path.get_path()), 'self.Ds', self.Ds)
+        # print('self.is_in_D', self.is_in_D)
+        self.is_in_D = True if self.current_position_idx >= len(self.path.get_path()) - self.Ds else False
         
     def back_home(self):
-		# send the pawn back to the home area. Necessary when a pawn is suspended by the opponent's one
+        # send the pawn back to the home area. Necessary when a pawn is suspended by the opponent's one
         self.current_position_idx = 0
         
     
--- Player.py
+++ Player.py
@@ -1,10 +1,24 @@
 from random import randint
 
 class Player:
-	def __init__(self, char):
-		self.char = char
-		
-		
-	def throw_die(self):
-		return randint(1, 6)
-	
+    def __init__(self, char, pawns):
+        self.char = char
+        self.pawns = pawns  
+        
+    def move_pawn(self, index_of_pawn, step):
+        self.pawns[index_of_pawn].move(step)
+        
+    def get_current_pawn_position(self, index_of_pawn):
+        self.pawns[index_of_pawn].get_current_position()
+        
+    def show_pawns_path(self):
+        for p in self.pawns:
+            print(p.show_pawns_path())
+    
+    def show_all_pawns_position(self):
+        for i, p in enumerate(self.pawns):
+            print('idx:', i, 'pos:', p.get_current_position())
+    
+    def throw_die(self):
+        return randint(1, 6)
+    
